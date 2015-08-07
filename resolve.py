0251a334ca7bf1d055c418d17dad6c34559813f2
Henrik Junklewitz <henrikju@astro.uni-bonn.de>
2015-08-07 17:20:21 +0200

    Fixed bug introduced by Lars

--- resolve.py
+++ resolve.py
@@ -1884,7 +1884,7 @@ def convert_CASA_to_RES(imagearray_fromCASA):
     """
     #with resepect to CASA, the imagearray is already rotated by 90 degrees
     #clockwise because of 0-point-shift between CASAIM/FITS and python.
-    return np.rot(np.transpose(imagearray_fromCASA),-1)
+    return np.rot90(np.transpose(imagearray_fromCASA),-1)
     
 def convert_RES_to_CASA(imagearray_fromRES,FITS=False):
     """
